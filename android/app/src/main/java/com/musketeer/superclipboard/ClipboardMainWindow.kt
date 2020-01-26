a3eea583a6f011009f11324cdd34b4db0ae641a1
zhongxuqi <zhongxuqi.1991@gmail.com>
2020-01-26 15:45:46 +0800

    add person settings window

--- android/app/src/main/java/com/musketeer/superclipboard/ClipboardMainWindow.kt
+++ android/app/src/main/java/com/musketeer/superclipboard/ClipboardMainWindow.kt
@@ -7,7 +7,9 @@ import android.os.Build
 import android.util.DisplayMetrics
 import android.view.*
 import android.view.View.OnTouchListener
+import android.widget.ImageView
 import android.widget.ListView
+import android.widget.PopupWindow
 import android.widget.Toast
 import com.musketeer.superclipboard.adapter.HistoryListAdapter
 import com.musketeer.superclipboard.data.ClipBoardMessage
@@ -18,6 +20,9 @@ import java.util.*
 class ClipboardMainWindow constructor(private val mContext: Context) {
     companion object {
         private var mContext: Context? = null
+        private var screenWidth: Int = 0
+        private var screenHeight: Int = 0
+
         private var mIsFloatViewShowing = false
         private var mWindowManager: WindowManager? = null
         private var mFloatView: View? = null
@@ -27,14 +32,17 @@ class ClipboardMainWindow constructor(private val mContext: Context) {
         private var mFloatViewLastY = 0
         private var mFloatViewFirstX = 0
         private var mFloatViewFirstY = 0
+        private var personSettingsBtn: ImageView? = null
 
         private var mContentListView: ListView? = null
         private var mContentListAdapter: HistoryListAdapter? = null
         private var mContentList: LinkedList<ClipBoardMessage> = LinkedList()
 
+        private var popWindow: PopupWindow? = null
+        private var personSettingsView: View? = null
 
         fun refreshAdapter() {
-            var msgList = SqliteHelper.helper!!.ListAll()
+            val msgList = SqliteHelper.helper!!.ListAll()
             mContentList.clear()
             mContentList.addAll(msgList)
             mContentListAdapter!!.notifyDataSetChanged()
@@ -114,6 +122,10 @@ class ClipboardMainWindow constructor(private val mContext: Context) {
         mWindowManager = mContext.getSystemService(WINDOW_SERVICE) as WindowManager
         val metrics = DisplayMetrics()
         mWindowManager!!.defaultDisplay.getMetrics(metrics)
+        screenWidth = metrics.widthPixels
+        screenHeight = metrics.heightPixels
+
+        // init base view
         val inflater = LayoutInflater.from(mContext)
         mFloatView = inflater.inflate(R.layout.clipboard_main_layout, null)
         mFloatView!!.setOnClickListener(mFloatViewOnClickListener)
@@ -123,13 +135,24 @@ class ClipboardMainWindow constructor(private val mContext: Context) {
         mFloatViewLayoutParams!!.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
         mFloatViewLayoutParams!!.type = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY else WindowManager.LayoutParams.TYPE_PHONE
         mFloatViewLayoutParams!!.gravity = Gravity.CENTER
-        mFloatViewLayoutParams!!.width = (metrics.widthPixels * 0.6).toInt()
-        mFloatViewLayoutParams!!.height = (metrics.heightPixels * 0.6).toInt()
+        mFloatViewLayoutParams!!.width = (screenWidth * 0.6).toInt()
+        mFloatViewLayoutParams!!.height = (screenHeight * 0.6).toInt()
+        personSettingsBtn = mFloatView!!.findViewById<ImageView>(R.id.person_settings)
 
-        var msgList = SqliteHelper.helper!!.ListAll()
+        // init history list
+        val msgList = SqliteHelper.helper!!.ListAll()
         mContentList.addAll(msgList)
         mContentListView = mFloatView!!.findViewById(R.id.history_list) as ListView
         mContentListAdapter = HistoryListAdapter(mContext, R.id.history_list_item_content, mContentList)
         mContentListView!!.adapter = mContentListAdapter
+
+        // init popup window
+        personSettingsView = inflater.inflate(R.layout.person_settings, null)
+        popWindow = PopupWindow(personSettingsView, (screenWidth * 0.4).toInt(), ViewGroup.LayoutParams.WRAP_CONTENT, true)
+        personSettingsBtn!!.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                popWindow!!.showAsDropDown(personSettingsBtn)
+            }
+        })
     }
 }
