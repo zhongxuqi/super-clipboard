ed45d470c8f8623c0bc034cac631bea1d523b7e5
zhongxuqi <zhongxuqi.1991@gmail.com>
2020-01-27 00:31:53 +0800

    add min window and new app icon

--- android/app/src/main/java/com/musketeer/superclipboard/ClipboardMainWindow.kt
+++ android/app/src/main/java/com/musketeer/superclipboard/ClipboardMainWindow.kt
@@ -10,7 +10,7 @@ import android.view.View.OnTouchListener
 import android.widget.ImageView
 import android.widget.ListView
 import android.widget.PopupWindow
-import android.widget.Toast
+import androidx.constraintlayout.widget.Constraints
 import com.musketeer.superclipboard.adapter.HistoryListAdapter
 import com.musketeer.superclipboard.data.ClipBoardMessage
 import com.musketeer.superclipboard.db.SqliteHelper
@@ -27,12 +27,13 @@ class ClipboardMainWindow constructor(private val mContext: Context) {
         private var mWindowManager: WindowManager? = null
         private var mFloatView: View? = null
         private var mFloatViewLayoutParams: WindowManager.LayoutParams? =null
-        private var mFloatViewTouchConsumedByMove = false
         private var mFloatViewLastX = 0
         private var mFloatViewLastY = 0
         private var mFloatViewFirstX = 0
         private var mFloatViewFirstY = 0
         private var personSettingsBtn: ImageView? = null
+        private var maxMainView: View? = null
+        private var minMainView: View? = null
 
         private var mContentListView: ListView? = null
         private var mContentListAdapter: HistoryListAdapter? = null
@@ -67,54 +68,52 @@ class ClipboardMainWindow constructor(private val mContext: Context) {
         }
     }
 
-    private val mFloatViewOnClickListener: View.OnClickListener = object : View.OnClickListener {
-        override
-        fun onClick(v: View?) {
-            Toast.makeText(
-                mContext,
-                "Float view is clicked!",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-    }
-
     private val mFloatViewOnTouchListener = OnTouchListener { v, event ->
+        var ret = false
         val prm = mFloatViewLayoutParams
-        val totalDeltaX = mFloatViewLastX - mFloatViewFirstX
-        val totalDeltaY = mFloatViewLastY - mFloatViewFirstY
         when (event.actionMasked) {
             MotionEvent.ACTION_DOWN -> {
                 mFloatViewLastX = event.rawX.toInt()
                 mFloatViewLastY = event.rawY.toInt()
                 mFloatViewFirstX = mFloatViewLastX
                 mFloatViewFirstY = mFloatViewLastY
+                ret = true
             }
             MotionEvent.ACTION_UP -> {
+                ret = true
             }
             MotionEvent.ACTION_MOVE -> {
                 val deltaX = event.rawX.toInt() - mFloatViewLastX
                 val deltaY = event.rawY.toInt() - mFloatViewLastY
                 mFloatViewLastX = event.rawX.toInt()
                 mFloatViewLastY = event.rawY.toInt()
-                if (Math.abs(totalDeltaX) >= 5 || Math.abs(totalDeltaY) >= 5) {
-                    if (event.pointerCount == 1) {
-                        prm!!.x += deltaX
-                        prm!!.y += deltaY
-                        mFloatViewTouchConsumedByMove = true
-                        if (mWindowManager != null) {
-                            mWindowManager!!.updateViewLayout(mFloatView, prm)
-                        }
-                    } else {
-                        mFloatViewTouchConsumedByMove = false
+                if (prm != null) {
+                    if (prm.x < 0) {
+                        prm.x = 0
+                    } else if (prm.x >= screenWidth - mFloatView!!.width) {
+                        prm.x = screenWidth - mFloatView!!.width
                     }
-                } else {
-                    mFloatViewTouchConsumedByMove = false
+                    if (prm.x + deltaX in 0 until screenWidth - mFloatView!!.width) {
+                        prm.x += deltaX
+                    }
+                    if (prm.y < 0) {
+                        prm.y = 0
+                    } else if (prm.y >= screenHeight - mFloatView!!.height) {
+                        prm.y = screenHeight - mFloatView!!.height
+                    }
+                    if (prm.y + deltaY in 0 until screenHeight - mFloatView!!.height) {
+                        prm.y += deltaY
+                    }
+                    if (mWindowManager != null) {
+                        mWindowManager!!.updateViewLayout(mFloatView, prm)
+                    }
                 }
+                ret = true
             }
             else -> {
             }
         }
-        mFloatViewTouchConsumedByMove
+        ret
     }
 
     init {
@@ -128,16 +127,34 @@ class ClipboardMainWindow constructor(private val mContext: Context) {
         // init base view
         val inflater = LayoutInflater.from(mContext)
         mFloatView = inflater.inflate(R.layout.clipboard_main_layout, null)
-        mFloatView!!.setOnClickListener(mFloatViewOnClickListener)
         mFloatView!!.setOnTouchListener(mFloatViewOnTouchListener)
+        personSettingsBtn = mFloatView!!.findViewById(R.id.person_settings)
+
+        maxMainView = mFloatView!!.findViewById(R.id.max_view)
+        maxMainView!!.layoutParams = Constraints.LayoutParams((screenWidth * 0.6).toInt(), (screenHeight * 0.6).toInt())
+        maxMainView!!.visibility = View.VISIBLE
+        maxMainView!!.findViewById<ImageView>(R.id.btn_window_min).setOnClickListener {
+            maxMainView!!.visibility = View.GONE
+            minMainView!!.visibility = View.VISIBLE
+        }
+
+
         mFloatViewLayoutParams = WindowManager.LayoutParams()
         mFloatViewLayoutParams!!.format = PixelFormat.TRANSLUCENT
         mFloatViewLayoutParams!!.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
         mFloatViewLayoutParams!!.type = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY else WindowManager.LayoutParams.TYPE_PHONE
-        mFloatViewLayoutParams!!.gravity = Gravity.CENTER
-        mFloatViewLayoutParams!!.width = (screenWidth * 0.6).toInt()
-        mFloatViewLayoutParams!!.height = (screenHeight * 0.6).toInt()
-        personSettingsBtn = mFloatView!!.findViewById<ImageView>(R.id.person_settings)
+        mFloatViewLayoutParams!!.width = WindowManager.LayoutParams.WRAP_CONTENT
+        mFloatViewLayoutParams!!.height = WindowManager.LayoutParams.WRAP_CONTENT
+        mFloatViewLayoutParams!!.gravity = Gravity.TOP or Gravity.LEFT
+        mFloatViewLayoutParams!!.x = screenWidth - maxMainView!!.layoutParams.width
+        mFloatViewLayoutParams!!.y = 0
+
+        minMainView = mFloatView!!.findViewById(R.id.min_view)
+        minMainView!!.visibility = View.GONE
+        minMainView!!.findViewById<ImageView>(R.id.btn_window_max).setOnClickListener {
+            maxMainView!!.visibility = View.VISIBLE
+            minMainView!!.visibility = View.GONE
+        }
 
         // init history list
         val msgList = SqliteHelper.helper!!.ListAll()
